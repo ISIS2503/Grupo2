217b1a1811e515a2cc1479125f3ce26a5a9e8072
jsprieto10 <js.prieto10@uniandes.edu.co>
2017-10-19 11:38:42 -0500

    cambios cassandra

--- minaSamaca/minaSamacaAPP/src/main/java/co/edu/uniandes/isis2503/nosqljpa/persistence/JPAConnection.java
+++ minaSamaca/minaSamacaAPP/src/main/java/co/edu/uniandes/isis2503/nosqljpa/persistence/JPAConnection.java
@@ -12,7 +12,7 @@ public class JPAConnection {
     public static final String CASSANDRA = "cassandra_db";
     public static final String DERBY = "derby_db";
 
-    public static final String DB = DERBY;
+    public static final String DB = CASSANDRA;
 
     private EntityManager entityManager;
     public static final JPAConnection CONNECTION = new JPAConnection();
@@ -20,7 +20,13 @@ public class JPAConnection {
     public JPAConnection() {
         if (entityManager == null) {
             EntityManagerFactory emf;
+            if (DB.equals(DERBY)) {
                 emf = Persistence.createEntityManagerFactory(DERBY);
+            } else {
+                Map<String, String> propertyMap = new HashMap<>();
+                propertyMap.put(CassandraConstants.CQL_VERSION, CassandraConstants.CQL_VERSION_3_0);
+                emf = Persistence.createEntityManagerFactory(CASSANDRA, propertyMap);
+            }
             entityManager = emf.createEntityManager();
         }
     }
